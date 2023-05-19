8eb1ace046debb040c9f19a7b4fde0541675fec3
hee <songyn111@gmail.com>
2023-05-19 16:50:54 +0900

    commit

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -42,7 +42,7 @@ class RestaurantScreen extends StatelessWidget {
                     itemBuilder: (_, index) {
                       final item = snapshot.data![index];
 
-                      final pItem = RestaurantModel.fromJson(json: item);
+                      final pItem = RestaurantModel.fromJson(item);
 
                       return GestureDetector(
                         onTap: () {
